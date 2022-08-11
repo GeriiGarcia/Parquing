03ad8422f502a3818db5737efb76df6af1c3c613
GeriiGarcia <gerardgarciagros@gmail.com>
2022-08-11 18:11:15 +0200

    Preparando

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    
+
+
+    return 0;
+}
+
+
+/* 
+
+Es un programa para controlar quien entra y sale del parquing y si se puede entrar o no.
+Si el parquing esta lleno, no se puede entrar.
+
+El parquing dipsone de 1000 plazas, 700 para Turismos, 100 para motos, 50 para vehiculos de familias numerosas, 100 para vehiculos grandes y 50 Son reservados para personal.
+
+Un Vehiculo de familia numerosa (VFN) puede entrar en las plazas para turismos, pero al contrario no.
+Los Vehiculos Grandes solo pueden entrar en sus plazas.
+Los Vechiculos reservados tienen un codigo de acceso que se le preguntara para dejarlo entrar, no tiene coste.
+
+
+
+
+
+
+
+
+
+
+
+*/ 
